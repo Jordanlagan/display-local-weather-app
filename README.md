bcafa731a1684f1b9a221c10ad594aefe1e19d14
Jordan Lagan <jordanlagan000@gmail.com>
2017-07-18 17:38:37 -0400

    fixed implementation errors and corrected the usage of the wrong service

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 This is an app I wrote for the FreeCodeCamp Intermediate challenges. It is also hosted on my codepen profile.
 	codepen: https://codepen.io/jordanlagan/pen/rwWYWW
 
-Using the Openweather API, you can enter a zip code and receive the current weather statistics in celsius or farenheit. 
+Using the simpleWeather library, you can enter a zip code and receive the current weather statistics in celsius or farenheit. 
 
-Feel free to use this code however you please, but I claim no ownership of the Openweather API or any services included thereof.
+Feel free to use this code however you please, but I claim no ownership of the simpleWeather or any services included thereof.
--- js/fetch-weather.js
+++ js/fetch-weather.js
@@ -1,26 +1,28 @@
+var unit = "f";
+
 $("#fetch-btn").click(function() {
-  var apiKey = "99c01db1ca0dc763c8c5c8c8ef901246";
-  var zipCode = document.getElementById("zip").value;
-  var baseURL = "https://cors.now.sh/http://api.openweathermap.org/data/2.5/weather";
+  if (document.getElementById("unit").checked) {
+  unit = "c";
+}
+else {
+  unit = "f";
+}
   
-  function getFullURL() {
-    return baseURL + "?zip=" + zipCode + ",us&APPID=" + apiKey;
-  };
-  
-  alert(getFullURL());
-  
-  $.ajax({
-    
-    type: "GET",
-    url: getFullURL(),
-    dataType: "jsonp",
-    success: function(results) {
-      alert('success');
-      $(".fetch").html('Results for: ' + zipCode + ": <br>");
-      $(".fetch").append(results.main.temp);
-    },
-    error: function() {
-    alert('error');
-  }
-  });
+  var loc = document.getElementById("zip").value;
+   $.simpleWeather({
+      location: loc,
+      woeid: '',
+      unit: unit,
+      success: function(weather) {
+        var html = '<p>'+weather.temp+'&deg;'+weather.units.temp+'</p>';
+
+        $(".fetch").html("Results for "+loc);
+        $(".fetch").append("<br>")
+        $(".fetch").append("<br>")
+        $(".fetch").append(html);
+      },
+      error: function(error) {
+        $(".fetch").html('<p>'+error+'</p>');
+      }
+    });
 });
